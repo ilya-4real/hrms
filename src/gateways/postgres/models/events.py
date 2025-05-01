bdb6eb0fa4b9e2ad6c2db83b5b538dff7c32e236
Ilya-4real <ilya.chernishyov@gmail.com>
2025-05-01 20:23:19 +0200

    fix: added oid field to event model

--- src/gateways/postgres/models/events.py
+++ src/gateways/postgres/models/events.py
@@ -9,12 +9,13 @@ from gateways.postgres.base import BaseORM
 class EventORM(BaseORM):
     __tablename__ = "events"
 
-    title: Mapped[str] = mapped_column(String(100), primary_key=True)
+    oid: Mapped[str] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
     starts_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
 
     @classmethod
     def from_entity(cls, event: Event) -> "EventORM":
-        return EventORM(title=event.title, starts_at=event.starts_at)
+        return EventORM(oid=event.oid, title=event.title, starts_at=event.starts_at)
 
     def to_entity(self) -> Event:
-        return Event(self.title, self.starts_at)
+        return Event(self.title, self.starts_at, oid=self.oid)
