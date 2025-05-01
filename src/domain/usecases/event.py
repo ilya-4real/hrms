16ff17f4797d507659a4e487bac9fc0d081f10bd
Ilya-4real <ilya.chernishyov@gmail.com>
2025-05-01 14:25:21 +0200

    feat: added events use cases

--- src/domain/usecases/event.py
+++ src/domain/usecases/event.py
@@ -1,13 +1,44 @@
 from dataclasses import dataclass
+from datetime import datetime
+from typing import override
 
 from domain.entities.event import Event
-from domain.usecases.base import BaseCommand, BaseUseCase
+from domain.interfaces import EventGateway
+from domain.usecases.base import BaseCommand, BaseUseCase, DBSession
 
 
 @dataclass
 class CreateEventCommand(BaseCommand):
-    event: Event
+    title: str
+    starts_at: datetime
 
 
 @dataclass
-class CreateEventUseCase(BaseUseCase): ...
+class CreateEventUseCase(BaseUseCase):
+    event_gateway: EventGateway
+    db_session: DBSession
+
+    @override
+    async def execute(self, command: CreateEventCommand) -> Event:
+        event = Event(command.title, command.starts_at)
+        event = await self.event_gateway.create_event(event)
+        await self.db_session.commit()
+        return event
+
+
+
+@dataclass
+class GetCurrentEvents(BaseCommand):
+    ...
+
+
+@dataclass
+class GetCurrentEventsUseCase(BaseUseCase):
+    event_gateway: EventGateway
+    db_session: DBSession
+
+    @override
+    async def execute(self, command: GetCurrentEvents) -> list[Event]:
+        events = await self.event_gateway.get_current_events()
+        await self.db_session.rollback()
+        return events
